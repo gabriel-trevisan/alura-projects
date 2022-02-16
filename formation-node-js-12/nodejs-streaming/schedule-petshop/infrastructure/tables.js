804fb5b07dc982ed0238667dd6e6d36b8a1a99d1
Gabriel Santos <gabriel.trevisan.santos@gmail.com>
2022-02-16 19:58:10 -0300

    Create method to create table pets

--- formation-node-js-12/nodejs-streaming/schedule-petshop/infrastructure/tables.js
+++ formation-node-js-12/nodejs-streaming/schedule-petshop/infrastructure/tables.js
@@ -3,12 +3,13 @@ class Tables {
         this.connection = connection;
 
         this.createServices();
+        this.createPets();
     }
 
     createServices(){
         const sql = 'CREATE TABLE IF NOT EXISTS services (id int NOT NULL AUTO_INCREMENT, customer varchar(50) NOT NULL, pet varchar(20), service varchar(20) NOT NULL, date datetime NOT NULL, creationDate datetime NOT NULL, status varchar(20) NOT NULL, comments text, PRIMARY KEY(id))'
 
-        this.connection.query(sql, (error, result) => {
+        this.connection.query(sql, error => {
             if(error){
                 console.log(error);
             } else {
@@ -16,6 +17,18 @@ class Tables {
             }
         })
 
+    }
+
+    createPets(){
+        const sql = "CREATE TABLE IF NOT EXISTS pets (id int NOT NULL AUTO_INCREMENT, name varchar(50), image varchar(200), PRIMARY KEY (id))"
+
+        this.connection.query(sql, error => {
+            if(error){
+                console.log(error);
+            } else {
+                console.log("Table 'pets' was created");
+            }
+        })
     }
 }
 

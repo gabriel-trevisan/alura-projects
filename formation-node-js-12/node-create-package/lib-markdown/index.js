3203ecc5cb85d41bff60beb21569a206b774f317
Gabriel Santos <gabriel.trevisan.santos@gmail.com>
2022-03-08 22:39:43 -0300

    Create function handle error

--- formation-node-js-12/node-create-package/lib-markdown/index.js
+++ formation-node-js-12/node-create-package/lib-markdown/index.js
@@ -1,9 +1,16 @@
 const chalk = require('chalk')
 const fs = require('fs')
 
+function handleError(error) {
+    throw new Error(error);
+}
+
 function readFile(path) {
     const encoding = 'utf-8'
-    fs.readFile(path, encoding, (_, data) => {
+    fs.readFile(path, encoding, (erro, data) => {
+        if(erro){
+            handleError(erro);
+        }
         console.log(chalk.green(data));
     })
 }

1746cd0a2f214601e503c0c05df3dcea96b82a93
Gabriel Santos <gabriel.trevisan.santos@gmail.com>
2022-03-10 19:20:26 -0300

    Change function readFile sync to async

--- formation-node-js-12/node-create-package/lib-markdown/index.js
+++ formation-node-js-12/node-create-package/lib-markdown/index.js
@@ -2,17 +2,15 @@ const chalk = require('chalk')
 const fs = require('fs')
 
 function handleError(error) {
-    throw new Error(error);
+    throw new Error(chalk.red(error, 'Path invalid!'));
 }
 
 function readFile(path) {
     const encoding = 'utf-8'
-    fs.readFile(path, encoding, (erro, data) => {
-        if(erro){
-            handleError(erro);
-        }
-        console.log(chalk.green(data));
-    })
+    fs.promises
+        .readFile(path, encoding)
+            .then(text => console.log(chalk.green(text)))
+            .catch(erro => handleError(erro))
 }
 
 readFile('./files/text.md');

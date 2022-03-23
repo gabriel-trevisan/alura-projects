37b254c1c435064362d8247380c08722bf707733
Gabriel Santos <gabriel.trevisan.santos@gmail.com>
2022-03-22 21:14:35 -0300

    Create function check status http and finish the program

--- formation-node-js-12/node-create-package/lib-markdown/cli.js
+++ formation-node-js-12/node-create-package/lib-markdown/cli.js
@@ -1,12 +1,13 @@
 const chalk = require('chalk')
 const readFile = require('./index')
+const validateURLs = require('./http-validation')
 
 const path = process.argv;
 
 async function processText(path){
     const result = await readFile(path[2]);
     if(path[3] === "validate") {
-        console.log(chalk.yellow('Validated links:'), result);
+        console.log(chalk.yellow('Validated links:'), await validateURLs(result));
     } else {
         console.log(chalk.yellow('Links:'), result);
     }
--- formation-node-js-12/node-create-package/lib-markdown/http-validation.js
+++ formation-node-js-12/node-create-package/lib-markdown/http-validation.js
@@ -1,5 +1,35 @@
-function validateURLs(arrayLinks){
-    return arrayLinks;
+const fetch = require('node-fetch')
+
+function handleError(error){
+    throw new Error(error.message);
+}
+
+async function checkStatus(arrayUrls){
+    try{
+        const arrayStatus = await Promise.all(arrayUrls.map(async url => {
+            const response = await fetch(url);
+            return response.status;
+        }));
+        return arrayStatus;
+    } catch (error){
+        handleError(error);
+    }
+}
+
+function createURLsArray(arrayLinks){
+    return arrayLinks.map(objectLink => Object.values(objectLink).join());
+}
+
+async function validateURLs(arrayLinks){
+    const links = createURLsArray(arrayLinks);
+    const statusLinks = await checkStatus(links);
+
+    const result = arrayLinks.map((object, index) => ({ 
+        ...object, 
+        status: statusLinks[index] 
+    }));
+
+    return result;
 }
 
 module.exports = validateURLs;

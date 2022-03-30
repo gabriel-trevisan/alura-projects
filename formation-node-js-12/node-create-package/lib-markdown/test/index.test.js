a883942f8897c5f77b157c4e19e868c6ca301dfe
Gabriel Santos <gabriel.trevisan.santos@gmail.com>
2022-03-29 21:43:50 -0300

    Finish tests

--- formation-node-js-12/node-create-package/lib-markdown/test/index.test.js
+++ formation-node-js-12/node-create-package/lib-markdown/test/index.test.js
@@ -1,7 +1,21 @@
 const readFile = require('../index')
 
+const arrayResult = [
+    {
+        FileList: 'https://developer.mozilla.org/pt-BR/docs/Web/API/FileList'
+    }
+]
+
 describe('readFile::', () => {
     it('must be a function', () => {
         expect(typeof readFile).toBe('function');
+    })
+    it('must return array with results::', async () => {
+        const result = await readFile('/home/gabriel/projects/alura-projects/formation-node-js-12/node-create-package/lib-markdown/test/files/text.md')
+        expect(result).toEqual(arrayResult);
+    })
+    it('must return message "Links not found"::', async () => {
+        const result = await readFile('/home/gabriel/projects/alura-projects/formation-node-js-12/node-create-package/lib-markdown/test/files/text_without_links.md')
+        expect(result).toBe('Links not found')
     })
 })
